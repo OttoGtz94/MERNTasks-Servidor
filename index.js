8c17a12d75ee31a6acc2543fd91aec9f26d57976
Otto Gutierrez <ogutierrezgz.94@hotmail.com>
2021-02-16 09:03:13 -0600

    Se crea servidor y archivopara las variables de entorno

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Index de servidor MERNTASKS');
+const express = require('express');
+
+// Crear servidor
+const app = express();
+// Puerto de la app
+const PORT = process.env.PORT || 4000;
+
+// Arrancar la app
+app.listen(PORT, () => {
+	console.log(`Servidor en el puerto: ${PORT}`);
+});
