a12d676f3b4d22545abb92ffd1bde703c04de83f
Otto Gutierrez <ogutierrezgz.94@hotmail.com>
2021-02-17 19:58:55 -0600

    Se crea fn para crear tareas

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -9,4 +9,29 @@ exports.crearTarea = async (req, res) => {
 	if (!errores.isEmpty()) {
 		return res.status(400).json({ errores: errores.array() });
 	}
+
+	try {
+		// extraer el proyecto
+		const { proyecto } = req.body;
+		// comprobar si existe
+		const existeProyecto = await Proyecto.findById(proyecto);
+		if (!existeProyecto) {
+			return res
+				.status(404)
+				.json({ msg: 'Proyecto no encontrado' });
+		}
+
+		// revisar si el proyecto actual pertenece al usuario autenticado
+		if (existeProyecto.creador.toString() !== req.usuario.id) {
+			return res.status(401).json({ msg: 'No autorizado' });
+		}
+
+		// crear la tarea
+		const tarea = new Tarea(req.body);
+		await tarea.save();
+		res.json({ tarea });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send('Error al crear tarea');
+	}
 };
